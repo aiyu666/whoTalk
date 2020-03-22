32f20f4389983c9cddb02a8c276312429daa95f0
RobinZhang <robinzhang@RobinZhangde-MacBook-Pro.local>
2020-03-22 18:46:53 +0800

    Add the GA reply

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var linebot = require('linebot');
 
 var bot = linebot({
-    channelId: CHANNEL_ID,
-    channelSecret: CHANNEL_SECRET,
-    channelAccessToken: CHANNEL_ACCESS_TOKEN
+    channelId: "1653974492",
+    channelSecret: "fabcb037585812a1f5c1dd956fd3b8aa",
+    channelAccessToken: "xCY575htizGkFZjqvwUawc0XVmM+oz3SV/CnBGdt9tE8TlGsYjmDh7bpkzHq7bTrRop0PltjlcqJzJLmB0VIKezMUKVySmqc4udTCoS6d/IotCD80JXxPjBPTyfv6BUTnJLnOgFLkE208JpClSViwwdB04t89/1O/w1cDnyilFU="
 });
 
 bot.on('message', function (event) {
-    event.reply(event.message.text).then(function (data) {
+    console.log(`I got the message -> ${event.message.text}`)
+    event.reply("尬~").then(function (data) {
         // success
-        console.log(data)
+        console.log(`Reply success`)
     }).catch(function (error) {
         // error
-        console.log(error)
+        console.log(`Some error message => ${error}`)
     });
 });
 
-bot.listen('/linewebhook', 3000);
+
+bot.listen('/linewebhook', 5000);
+console.log("Listening...5000 port")
