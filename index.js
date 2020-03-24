cf20dbd3a0aa39fdcd21fd560823040ed650f6fc
RobinZhang <robinzhang@RobinZhangde-MacBook-Pro.local>
2020-03-25 07:08:40 +0800

    Add connect MongoDB

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-var linebot = require('linebot');
+const linebot = require('linebot');
+const MongoDB = require('./module/connectMongoDB');
+// const getProfile = require('./module/getProfile');
 
-var bot = linebot({
+const bot = linebot({
     channelId: "1653974492",
     channelSecret: "fabcb037585812a1f5c1dd956fd3b8aa",
     channelAccessToken: "xCY575htizGkFZjqvwUawc0XVmM+oz3SV/CnBGdt9tE8TlGsYjmDh7bpkzHq7bTrRop0PltjlcqJzJLmB0VIKezMUKVySmqc4udTCoS6d/IotCD80JXxPjBPTyfv6BUTnJLnOgFLkE208JpClSViwwdB04t89/1O/w1cDnyilFU="
 });
 
-bot.on('message', function (event) {
-    console.log(`I got the message -> ${event.message.text}`)
-    event.reply(`${JSON.stringify(event)}`).then(function (data) {
-        // success
-        console.log(`Reply success`)
-    }).catch(function (error) {
-        // error
-        console.log(`Some error message => ${error}`)
-    });
+
+bot.on('message', async function (event) {
+    const profile = await bot.getUserProfile(event.source.userId);
+    await console.log(`I got ${profile.displayName} say the message -> ${event.message.text}`)
+    // const db = await mongo.connectMongo();
+    // await mongo.close(db);
+
+    // await console.log(`${JSON.stringify(event)}`)
+    // await console.log(`${JSON.stringify(event.source.userId)}`)
+
+    const queryData = await MongoDB.queryData();
+    // await event.reply(`@${profile.displayName} : ${event.message.text}`)
+    await event.reply(`尬~${queryData}`)
+
+    await console.log(`Reply success`)
 });
 bot.on('join', function (event) {
-    console.log(`I got the join -> ${JSON.stringify(event)}`)
+    const member = bot.getGroupMember(event.source.groupId)
+    console.log(`I got the join -> ${JSON.stringify(member.memberIds)}`)
 });
 bot.on('leave', function (event) {
     console.log(`I got the leave -> ${JSON.stringify(event)}`)
@@ -35,9 +44,6 @@ bot.on('memberJoined', function (event) {
 });
 
 
-
-
-
-
 bot.listen('/linewebhook', 5000);
 console.log("Listening...5000 port")
+
---
+++ module/connectMongoDB.js
@@ -0,0 +1,51 @@
+
+var MongoClient = require('mongodb').MongoClient;
+
+async function connectMongo() {
+    const db = await MongoClient.connect("mongodb://localhost:27017/whoTalk", { useUnifiedTopology: true }).catch(err => {
+        console.log(`Some error with => ${err}`)
+        throw err;
+    });
+    console.log(`Connect success =>`)
+    return db.db('whoTalk')
+};
+
+async function closeDB(db) {
+    db.close();
+    console.log(`closeDB success`);
+    return
+};
+
+async function inserData(data) {
+    const db = await connectMongo();
+    const collection = await db.collection('whoTalk');
+    await collection.insert(data);
+    console.log(`Insert data sucess`)
+    closeDB(db);
+    return
+}
+
+async function queryData() {
+    const db = await connectMongo();
+    const collection = await db.collection('whoTalk');
+    const queryData = await collection.find({}).toArray();
+    await console.log(JSON.stringify(queryData))
+    await console.log(`qerry success`);
+    return JSON.stringify(queryData);
+}
+
+// async function queryData(data) {
+//     const db = await connectMongo();
+//     const collection = await db.collection('whoTalk');
+//     const queryData = await collection.find(data);
+//     console.log(`qerry success`);
+//     closeDB(db);
+//     return queryData
+// }
+
+module.exports = {
+    connectMongo,
+    closeDB,
+    inserData,
+    queryData
+}
