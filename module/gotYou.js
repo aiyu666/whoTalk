f33173a8179eead3e91d2054555c126f3b9392ef
RobinZhang <robinzhang@RobinZhangde-MacBook-Pro.local>
2020-03-28 17:39:02 +0800

    Update npm config

--- module/gotYou.js
+++ module/gotYou.js
@@ -5,7 +5,7 @@ module.exports = async function (groupId) {
     const collection = await db.collection;
     const queryData = await collection.find({ "groupId": groupId }).sort({ _id: -1 }).limit(1).toArray().catch(err => console.log(`Got some error from query data in got you ->${err}`));
     await console.log(`抓到了！！！`)
-    const msg = await `尬~他剛剛說 "${queryData[0].message}"! \n當本尬是塑膠逆？`
+    const msg = await `尬~他剛剛說 \n"${queryData[0].message}"! \n當本尬是塑膠逆？`
     console.log(`我在抓到了這邊處理了這個${msg}`)
     return msg
 }
