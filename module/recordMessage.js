5c17d40059bd7b1938f08df83bcc28756f5dd8ae
RobinZhang <robinzhang@RobinZhangde-MacBook-Pro.local>
2020-03-30 23:19:43 +0800

    Add sticker feature

--- module/recordMessage.js
+++ module/recordMessage.js
@@ -7,10 +7,24 @@ async function textMessage(groupId, userId, name, message, timestamp) {
             "userId": userId,
             "name": name,
             "message": message,
-            "tag": "userMessage",
+            "tag": "userTextMessage",
             "timestamp": timestamp
         });
 }
+
+async function stickerMessage(groupId, userId, name, stickerId, timestamp) {
+    return await MongoDB.insertData(
+        {
+            "groupId": groupId,
+            "userId": userId,
+            "name": name,
+            "stickerId": stickerId,
+            "tag": "userStickerMessage",
+            "timestamp": timestamp
+        });
+}
+
 module.exports = {
-    textMessage
+    textMessage,
+    stickerMessage
 }
