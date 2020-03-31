a84e487a30a77c7e8ef06a1a14be2f373211cd08
dang113108 <dang113108@gmail.com>
2020-03-31 18:47:01 +0800

    Shit QQ

--- module/gotYou.js
+++ module/gotYou.js
@@ -4,7 +4,7 @@ module.exports = async function(groupId) {
 
     const db = await MongoDB.connectMongo();
     const collection = await db.collection;
-    const queryData = await collection.find({ "groupId": groupId }).sort({ _id: -1 }).limit(1).toArray().catch(err => console.log("Got some error from query data in got you ->${err}"));
+    const queryData = await collection.find({ "groupId": groupId }).sort({ _id: -1 }).limit(1).toArray().catch(err => console.log(`Got some error from query data in got you ->${err}`));
     await console.log("抓到了！！！")
     if (queryData[0].tag === "userTextMessage") {
         const msg = await queryData[0].message;
