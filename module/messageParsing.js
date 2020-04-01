83be4fad23419590f7b40e1a106c2cf955312986
Robert Chen <robertchen@exosite.com>
2020-04-01 15:59:11 +0800

    Update to avoid catch self message

--- module/messageParsing.js
+++ module/messageParsing.js
@@ -2,7 +2,7 @@ const recordMessage = require("./recordMessage");
 const gotYou = require("./gotYou");
 
 async function messageSelector(replyToken, groupId, userId, name, message, timestamp) {
-    if (message === "抓") return await gotYou(groupId);
+    if (message === "抓") return await gotYou(groupId, userId);
     if (message === "抽") return ["pick", "抽你老木 林北不是Orz"];
     console.log(replyToken);
     return recordMessage.textMessage(replyToken, groupId, userId, name, message, timestamp);
