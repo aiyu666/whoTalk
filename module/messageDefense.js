929a8ee520e2bff3225abd759a6bdafb70030f97
Robert Chen <robertchen@exosite.com>
2020-04-01 22:28:34 +0800

    Improve defense code verify function

--- module/messageDefense.js
+++ module/messageDefense.js
@@ -3,8 +3,8 @@ const MongoDB = require("./connectMongoDB");
 async function verifyDefenseStatus(timestamp, defenseCode) {
     const db = await MongoDB.connectMongo();
     const collection = await db.collection;
-    const defenseData = await collection.find({ "timestamp": { $gt: timestamp }, "message": defenseCode }).toArray().catch(err => console.log(`Got some error from query defense code in messageDefense ->${err}`));
-    if (defenseData.length === 0) return false
+    const defenseData = await collection.updateMany({ "timestamp": { $gt: timestamp }, "message": defenseCode }, { $set: { "catch": defenseCode } }).catch(err => console.log(`Got some error from query defense code in messageDefense ->${err}`));
+    if (defenseData.matchedCount === 0) return false
     return true
 }
 
