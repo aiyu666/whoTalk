c1b8e64773672ed9cc33e675e4f14f94b3ac742f
Robert Chen <robertchen@exosite.com>
2020-04-02 19:48:20 +0800

    Hotfox for port error

--- app.js
+++ app.js
@@ -124,5 +124,5 @@ bot.on("memberJoined", function(event) {
 const linebotParser = bot.parser();
 app.post('/linewebhook', linebotParser);
 app.use(express.static("public"));
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
 console.log("Listening...5000 port")
