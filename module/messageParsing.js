aefa82a229a7a9ec4b811adffa6d4afe0674950d
Robert Chen <robert.chen@westpharma.com>
2020-08-11 23:17:56 +0800

    Catch Ron

--- module/messageParsing.js
+++ module/messageParsing.js
@@ -1,8 +1,11 @@
 const recordMessage = require("./recordMessage");
 const gotYou = require("./gotYou");
+const ronKeyword = ["抓ron", "抓Ron"];
+const ronID = "Ub0d1d64651a42ab071b4231dec86cae5";
 
 async function messageSelector(replyToken, groupId, userId, name, message, timestamp) {
     if (message === "抓") return await gotYou(groupId, userId);
+    if (ronKeyword.includes(message)) return await gotYou(groupId, ronID);
     if (message === "抽") return ["pick", "抽你老木 林北不是Orz"];
     console.log(replyToken);
     await recordMessage.textMessage(replyToken, groupId, userId, name, message, timestamp);
