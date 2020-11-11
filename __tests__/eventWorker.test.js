aed7379438ed4fb80d153f1435d04688f08a993e
aiyu666 <zrobin8741@gmail.com>
2020-11-12 02:01:57 +0800

    Stage

---
+++ __tests__/eventWorker.test.js
@@ -0,0 +1,12 @@
+// require('dotenv').config();
+// const { pickPhoto } = require('./pick');
+
+// const { gotYouEvent, pickEvent } = require('../lib/eventWorker');
+
+// jest.mock('pickPhoto');
+
+// describe('Test eventWorker', () => {
+//   test('Can pick random photo', async () => {
+//     pickPhoto.mockResolvedValue({ 'event': { reply:  } });
+//   });
+// });
