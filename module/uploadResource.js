a252e260357ab002f193001d04634b6215b3d428
Robert Chen <robert.chen@amazingtalker.com>
2022-04-26 18:36:08 +0800

    fix: Update the deprecated buffer function to upload image

--- module/uploadResource.js
+++ module/uploadResource.js
@@ -1,9 +1,9 @@
 const Buffer = require('buffer').Buffer;
 
 async function saveImage(data, filename) {
-    let imageBuffer = new Buffer(data, 'base64');
+    let imageBuffer = Buffer.from(data, 'base64');
 
-    await require('fs').writeFile('./public/img/' + filename, imageBuffer, function(err) {
+    await require('fs').writeFile('./public/img/' + filename, imageBuffer, function (err) {
         if (err) console.error(err);
         console.log(`file ${filename} saved.`);
     });
